eae6174a3052f0df05384bb9f2777f520dfd5004
Li-Deng-Hui <li_deng_hui_123@163.com>
2023-12-08 10:00:02 +0800

    Default Changelist

--- componentProjetWeb/webvue/src/routers/index.js
+++
@@ -1,9 +0,0 @@
-import ComponentIndex from '../pages/index';
-import UserLogin from '../pages/user/Login/index'
-
-const routers=[
-    {path:'/',name:'componentIndex',component:ComponentIndex},
-    {path: '/user/login',name:'userLogin',component: UserLogin}
-]
-
-export default routers;
